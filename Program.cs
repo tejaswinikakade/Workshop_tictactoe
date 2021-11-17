aa60e4887ef6038b8080e1d7574d217ff56ef80a
tejaswini kakade <92703176+tejaswinikakade@users.noreply.github.com>
2021-11-17 09:59:12 +0530

    UC1_TICTACTOE

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Play the game TIC_TAC_TOE");
+            UC1_CreatBoard uc1 = new UC1_CreatBoard();
+            uc1.Board();
+        }
+    }
+}
---
+++ UC1_CreateBoard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+    public class UC1_CreatBoard
+    {
+
+        char[] arr = new char[10];
+
+        public void Board()
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+    }
+}
